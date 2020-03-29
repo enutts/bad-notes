bff3e5942a8a65f7044173302998697801bdb4aa
Eric Nutter <nutts@pop-os.localdomain>
2020-03-29 15:55:07 -0500

    ver 0.1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bad-notes
+
+# goal
+this will perform some management of files in your notes directory.
--- notes.py
+++ notes.py
@@ -23,7 +23,30 @@ def search(search_string, notes, case=False):
                         return (note, line)
 
 def main(): 
+    # Options
+    case = False
     
+    if len(sys.argv) == 1:
+        print(docstring)
+        return
+    elif len(sys.argv) >= 2:
+        if '-h' in sys.argv or '--help' in sys.argv:
+            print(docstring)
+            return
+        if '-c' in sys.argv:
+            case = True
+            sys.argv.pop(sys.argv.index('-c'))
+    else:
+        print('Something went wrong')
+        return
+
+    if len(sys.argv) == 1: 
+        if case == False
+            search(sys.argv[0])
+        else:
+            search(sys.argv[0], case=True)
+    else:
+        print('you passed too many arguments')
 
 if __name__ == '__main__':
     main()
